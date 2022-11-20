f6590e50397efbaa5d8a545ccd657fb458aa51b7
Navnish Singh <navnish.singh@synergymachines.us>
2022-11-20 22:24:53 +0530

    with clustors

--- src/index.js
+++ src/index.js
@@ -7,52 +7,52 @@ const apiRoutes = require("./apiRoutes.json");
 const responseFunctions = require("./requestHandlers");
 
 // Check if the current process is the master process
-// if (cluster.isPrimary) {
-//   const numCPUs = require("os").cpus().length;
+if (cluster.isPrimary) {
+  const numCPUs = require("os").cpus().length;
 
-//   console.log(`Primary ${process.pid} is running`);
+  console.log(`Primary ${process.pid} is running`);
 
-//   // Fork workers.
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-//   cluster.on("online", (worker) => {
-//     console.log(`Worker ${worker.process.pid} is online`);
-//   });
-
-//   cluster.on('exit', (worker) => {
-//     console.log(`Worker ${worker.process.pid} died. Restarting...`);
-//     cluster.fork();
-//   });
-
-// } else {
-
-const { connectToDB } = require("./db");
-
-// Connect to mongo db
-connectToDB().then(() => {
-  // Create a server object
-  const server = http.createServer((req, res) => {
-
-    res.setHeader('Access-Control-Allow-Origin', "*");
-
-    req.setEncoding("utf8");
-
-    const responseFunctionKey = apiRoutes[req.method]?.[req.url];
-
-    if (responseFunctionKey) {
-      const responseFunction = responseFunctions[responseFunctionKey];
-      responseFunction(req, res);
-    } else {
-      res.writeHead(404, JSON_TYPE);
-      res.end(JSON.stringify({ message: RESPONSE_MESSAGES.NOT_FOUND }));
-    }
+  cluster.on("online", (worker) => {
+    console.log(`Worker ${worker.process.pid} is online`);
   });
 
-  // Start the server on port 3003
-  server.listen(3003, () => {
-    console.log(`Server running on port 3003 with process id ${process.pid}`);
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} died. Restarting...`);
+    cluster.fork();
   });
-});
-// }
+
+} else {
+
+  const { connectToDB } = require("./db");
+
+  // Connect to mongo db
+  connectToDB().then(() => {
+    // Create a server object
+    const server = http.createServer((req, res) => {
+
+      res.setHeader('Access-Control-Allow-Origin', "*");
+
+      req.setEncoding("utf8");
+
+      const responseFunctionKey = apiRoutes[req.method]?.[req.url];
+
+      if (responseFunctionKey) {
+        const responseFunction = responseFunctions[responseFunctionKey];
+        responseFunction(req, res);
+      } else {
+        res.writeHead(404, JSON_TYPE);
+        res.end(JSON.stringify({ message: RESPONSE_MESSAGES.NOT_FOUND }));
+      }
+    });
+
+    // Start the server on port 3003
+    server.listen(3003, () => {
+      console.log(`Server running on port 3003 with process id ${process.pid}`);
+    });
+  });
+}
